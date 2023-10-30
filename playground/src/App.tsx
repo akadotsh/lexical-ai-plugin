e2b5f545d93ac1b1aaf07d9c8da34be9fdfecd6b
akash-frontier <akash@frontier.xyz>
2023-10-30 23:59:55 +0530

    init

---
+++ playground/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Editor from "./components/Editor";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>Lexical Editor</h1>
+      <Editor />
+    </>
+  );
+}
+
+export default App;
---
+++ playground/src/components/Editor.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+const theme = {};
+
+function MyCustomAutoFocusPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    editor.focus();
+  }, [editor]);
+
+  return null;
+}
+
+function onError(error: unknown) {
+  console.error(error);
+}
+
+function Editor() {
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+      <MyCustomAutoFocusPlugin />
+    </LexicalComposer>
+  );
+}
+
+export default Editor;
